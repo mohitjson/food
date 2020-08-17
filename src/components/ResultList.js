20a25fa3fad3b4c9f771494cc0bd72eb88f02cc3
mohitjson <mohitjson@gmail.com>
2020-08-17 18:57:19 +0530

    commit

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,41 +1,46 @@
 import React from 'react';
-import{View,Text,StyleSheet,FlatList} from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import ResultDetail from './ResultsDetail';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import {withNavigation} from 'react-navigation';
+import { withNavigation } from 'react-navigation';
 
-const ResultList =({title,results,navigation})=>{
-    return(
+const ResultList = ({ title, results, navigation }) => {
+  if(!results.length){
+      return null;
+  }
+  
+    return (
         <View style={styles.title}>
             <Text style={styles.title}>{title} </Text>
             <FlatList
 
-            horizontal={true}
-            showsHorizontalScrollIndicator={false}
-            data={results}
-            keyExtractor={(results)=>results.id}
-            renderItem={({item}) =>{
-                return(
-                <TouchableOpacity onPress={()=>navigation.navigate('ResultsShow'),{id:item.id}}>
-                 <ResultDetail result={item} />
-                </TouchableOpacity>               
-              )}}
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                data={results}
+                keyExtractor={(results) => results.id}
+                renderItem={({ item }) => {
+                    return (
+                        <TouchableOpacity onPress={() => navigation.navigate('ResultsShow', { id: item.id })}>
+                            <ResultDetail result={item} />
+                        </TouchableOpacity>
+                    )
+                }}
             />
 
         </View>
     )
 }
-const styles= StyleSheet.create({
-    title:{
-        fontSize:18,
-        fontWeight:'bold',
-        marginLeft:15,
-        marginBottom:5,
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginLeft: 15,
+        marginBottom: 5,
     },
-    container:{
-        marginBottom:10,
+    container: {
+        marginBottom: 10,
 
     }
 });
 
- export default withNavigation(ResultList);
+export default withNavigation(ResultList);
